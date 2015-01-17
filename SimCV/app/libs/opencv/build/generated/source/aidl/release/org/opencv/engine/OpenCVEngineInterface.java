b3fb9bfa8ea6c5fbb3f58ee04c381b4a7715138a
simjes <s.jespersen@stud.uis.no>
2015-01-17 16:58:08 +0100

    open camera feed

--- SimCV/app/libs/opencv/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ SimCV/app/libs/opencv/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Simon\\AndroidStudioProjects\\SimCV\\app\\libs\\opencv\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Simon\\OpenCV-Bachelor\\SimCV\\app\\libs\\opencv\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
--- SimCV/app/src/main/java/com/simjessimsol/simcv/LauncherActivity.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/LauncherActivity.java
@@ -2,17 +2,79 @@ package com.simjessimsol.simcv;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class LauncherActivity extends ActionBarActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class LauncherActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private CameraBridgeViewBase cameraView;
+    private final static String TAG = "com.simjessimsol.simcv";
+    private int cameraIndex = 0;
+
+    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    Log.d(TAG, "OpenCV loaded successfully");
+                    cameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launcher);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        cameraView = new JavaCameraView(this, cameraIndex);
+        cameraView.setCvCameraViewListener(this);
+        cameraView.enableFpsMeter();
+        setContentView(cameraView);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (cameraView != null) {
+            cameraView.disableView();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV Manager used");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, loaderCallback);
+        } else {
+            Log.d(TAG, "Found OpenCV in the app");
+            loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,5 +96,21 @@ public class LauncherActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat rgba = inputFrame.rgba();
+        return rgba;
     }
 }
