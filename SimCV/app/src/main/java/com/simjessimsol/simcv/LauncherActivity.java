4468ee24db1e02a3b2ac22f84aee5d80abc07589
simjes <s.jespersen@stud.uis.no>
2015-01-20 09:40:58 +0100

    rem imports

--- SimCV/app/src/main/java/com/simjessimsol/simcv/LauncherActivity.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/LauncherActivity.java
@@ -8,8 +8,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
