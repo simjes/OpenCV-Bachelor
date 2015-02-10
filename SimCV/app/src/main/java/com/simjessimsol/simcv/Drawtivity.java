bca6864afc661d6016e9232af947af486be451fa
simjes <s.jespersen@stud.uis.no>
2015-02-10 14:54:37 +0100

    RGB tracking, maybe tweak light sensitivity

--- SimCV/app/src/main/java/com/simjessimsol/simcv/Drawtivity.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/Drawtivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.SeekBar;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -26,37 +27,39 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
     private CameraBridgeViewBase cameraView;
     private Mat hsvFrame;
     private Mat inOutFrame;
-    private Mat storeYellowPoints;
+    private Mat storeRedPoints;
     private Mat storeBluePoints;
+    private Mat storeGreenPoints;
     private Mat drawingMat;
 
-    private Scalar lowYellow = new Scalar(20, 100, 100);
-    private Scalar highYellow = new Scalar(30, 255, 255);
-    private Scalar lowBlue = new Scalar(100, 100, 100);
-    private Scalar highBlue = new Scalar(110, 255, 255);
+    private Scalar lowRed = new Scalar(163, 191, 211);
+    private Scalar highRed = new Scalar(180, 255, 255);
+    private Scalar lowBlue = new Scalar(87, 200, 185);
+    private Scalar highBlue = new Scalar(104, 255, 255);
+    private Scalar lowGreen = new Scalar(38, 108, 125);
+    private Scalar highGreen = new Scalar(73, 255, 255);
 
-    static int yellowPosX = 0;
-    static int yellowPosY = 0;
+    static int redPosX = 0;
+    static int redPosY = 0;
     static int bluePosX = 0;
     static int bluePosY = 0;
+    static int greenPosX = 0;
+    static int greenPosY = 0;
 
-    /*public int lowhue = 0;
+    /*
+    public int lowhue = 0;
     public int lowsat = 0;
     public int lowval = 0;
-
     public int highhue = 0;
     public int highsat = 0;
     public int highval = 0;
-
     private SeekBar hueLowSeekBar;
     private SeekBar saturationLowSeekBar;
     private SeekBar valueLowSeekBar;
-
     private SeekBar hueHighSeekBar;
     private SeekBar saturationHighSeekBar;
-    private SeekBar valueHighSeekBar;*/
-
-
+    private SeekBar valueHighSeekBar;
+    */
     private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -76,14 +79,13 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_drawtivity);
 
-        /*hueLowSeekBar = (SeekBar) findViewById(R.id.hueLowSeekBar);
+        /*
+        hueLowSeekBar = (SeekBar) findViewById(R.id.hueLowSeekBar);
         saturationLowSeekBar = (SeekBar) findViewById(R.id.saturationLowSeekBar);
         valueLowSeekBar = (SeekBar) findViewById(R.id.valueLowSeekBar);
-
         hueHighSeekBar = (SeekBar) findViewById(R.id.hueHighSeekBar);
         saturationHighSeekBar = (SeekBar) findViewById(R.id.saturationHighSeekBar);
         valueHighSeekBar = (SeekBar) findViewById(R.id.valueHighSeekBar);
-
         hueLowSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressVal = 0;
 
@@ -94,7 +96,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -102,7 +103,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
                 lowhue = progressVal;
             }
         });
-
         saturationLowSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressVal = 0;
 
@@ -113,7 +113,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -121,7 +120,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
                 lowsat = progressVal;
             }
         });
-
         valueLowSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressVal = 0;
 
@@ -132,7 +130,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -140,7 +137,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
                 lowval = progressVal;
             }
         });
-
         hueHighSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressVal = 0;
 
@@ -151,7 +147,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -159,7 +154,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
                 highhue = progressVal;
             }
         });
-
         saturationHighSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressVal = 0;
 
@@ -170,7 +164,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
@@ -178,7 +171,6 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
                 highsat = progressVal;
             }
         });
-
         valueHighSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progressVal = 0;
 
@@ -189,14 +181,14 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 highval = progressVal;
             }
-        });*/
+        });
+        */
 
         cameraView = (CameraBridgeViewBase) findViewById(R.id.drawcam);
         cameraView.setVisibility(SurfaceView.VISIBLE);
@@ -236,8 +228,9 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
     public void onCameraViewStarted(int width, int height) {
         hsvFrame = new Mat();
         inOutFrame = new Mat();
-        storeYellowPoints = new Mat(new Size(width, height), 1);
+        storeRedPoints = new Mat(new Size(width, height), 1);
         storeBluePoints = new Mat(new Size(width, height), 1);
+        storeGreenPoints = new Mat(new Size(width, height), 1);
         drawingMat = new Mat(new Size(width, height), CvType.CV_8UC4);
     }
 
@@ -245,7 +238,7 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
     public void onCameraViewStopped() {
         hsvFrame.release();
         inOutFrame.release();
-        storeYellowPoints.release();
+        storeRedPoints.release();
         storeBluePoints.release();
         drawingMat.release();
     }
@@ -256,32 +249,31 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
         Imgproc.cvtColor(inOutFrame, hsvFrame, Imgproc.COLOR_RGB2HSV);
 
 
-        Core.inRange(hsvFrame, lowYellow, highYellow, storeYellowPoints);
+        Core.inRange(hsvFrame, lowRed, highRed, storeRedPoints);
         Core.inRange(hsvFrame, lowBlue, highBlue, storeBluePoints);
-        //Imgproc.erode(storeYellowPoints, storeYellowPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
-        //Imgproc.erode(storeYellowPoints, storeYellowPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
-        //Imgproc.dilate(storeYellowPoints, storeYellowPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(8, 8)));
-        //Imgproc.dilate(storeYellowPoints, storeYellowPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(8, 8)));
+        Core.inRange(hsvFrame, lowGreen, highGreen, storeGreenPoints);
+        //Imgproc.erode(storeRedPoints, storeRedPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
+        //Imgproc.erode(storeRedPoints, storeRedPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
+        //Imgproc.dilate(storeRedPoints, storeRedPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(8, 8)));
+        //Imgproc.dilate(storeRedPoints, storeRedPoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(8, 8)));
         //Imgproc.erode(storeBluePoints, storeBluePoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
         //Imgproc.erode(storeBluePoints, storeBluePoints, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3)));
 
 
-        //yellow
-        Moments yellowMoments = Imgproc.moments(storeYellowPoints, true);
-        double yellowMoment10 = yellowMoments.get_m10();
-        double yellowMoment01 = yellowMoments.get_m01();
-        double yellowArea = yellowMoments.get_m00();
+        //red
+        Moments redMoments = Imgproc.moments(storeRedPoints, true);
+        double redMoment10 = redMoments.get_m10();
+        double redMoment01 = redMoments.get_m01();
+        double redArea = redMoments.get_m00();
 
-        int yellowLastX = yellowPosX;
-        int yellowLastY = yellowPosY;
+        int redLastX = redPosX;
+        int redLastY = redPosY;
 
-        yellowPosX = (int) (yellowMoment10 / yellowArea);
-        yellowPosY = (int) (yellowMoment01 / yellowArea);
+        redPosX = (int) (redMoment10 / redArea);
+        redPosY = (int) (redMoment01 / redArea);
 
-        Log.i(TAG, "posx: " + yellowPosX + ", posy: " + yellowPosY + ", yellowLastx: " + yellowLastX + ", yellowLasty: " + yellowLastY);
-        Log.i(TAG, "drawmat: " + drawingMat.size() + ", " + drawingMat.channels() + ", tmpframe: " + inOutFrame.size() + ", " + inOutFrame.channels());
-        if (yellowLastX > 0 && yellowLastY > 0 && yellowPosX > 0 && yellowPosY > 0) {
-            Core.line(drawingMat, new Point(yellowPosX, yellowPosY), new Point(yellowLastX, yellowLastY), new Scalar(255, 255, 0), 10);
+        if (redLastX > 0 && redLastY > 0 && redPosX > 0 && redPosY > 0) {
+            Core.line(drawingMat, new Point(redPosX, redPosY), new Point(redLastX, redLastY), new Scalar(255, 0, 0), 10);
         }
 
         //Blue
@@ -300,10 +292,29 @@ public class Drawtivity extends Activity implements CameraBridgeViewBase.CvCamer
             Core.line(drawingMat, new Point(bluePosX, bluePosY), new Point(blueLastX, blueLastY), new Scalar(0, 0, 255), 10);
         }
 
+        //Green
+        Moments greenMoments = Imgproc.moments(storeGreenPoints, true);
+        double greenMoment10 = greenMoments.get_m10();
+        double greenMoment01 = greenMoments.get_m01();
+        double greenArea = greenMoments.get_m00();
+
+        int greenLastX = greenPosX;
+        int greenLastY = greenPosY;
+
+        greenPosX = (int) (greenMoment10 / greenArea);
+        greenPosY = (int) (greenMoment01 / greenArea);
+        if (greenPosX > 0 && greenPosY > 0 && greenLastX > 0 && greenLastY > 0) {
+            Core.line(drawingMat, new Point(greenPosX, greenPosY), new Point(greenLastX, greenLastY), new Scalar(0, 255, 0), 10);
+        }
+
         Core.add(inOutFrame, drawingMat, inOutFrame);
 
-        //return storeBluePoints;
+        //return storeRedPoints;
         return inOutFrame;
+
+        /*Core.inRange(hsvFrame, new Scalar(lowhue, lowsat, lowval), new Scalar(highhue, highsat, highval), hsvFrame);
+        Log.i(TAG, "lowHue: " + lowhue + ", lowSat: " + lowsat + ", lowVal: " + lowval + "....highHue: " + highhue + ", highSat: " + highsat + ", highVal: " + highval);
+        return hsvFrame;*/
     }
 
 }
