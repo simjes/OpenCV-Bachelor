401924935b6d66ab18661ec5f4105157a115078a
solfuglen <simen.sollie@gmail.com>
2015-02-26 00:53:13 +0100

    added methods for initiating functions

--- SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/MainMenuActivity.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/MainMenuActivity.java
@@ -1,13 +1,23 @@
 package com.simjessimsol.simcv.menu_main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import com.simjessimsol.simcv.CircleDetection;
+import com.simjessimsol.simcv.Drawtivity;
+import com.simjessimsol.simcv.FaceDetection;
 import com.simjessimsol.simcv.R;
-
-import java.util.HashMap;
+import com.simjessimsol.simcv.foregroundDetection;
+import com.simjessimsol.simcv.nonopencv.ColorTrackerNonOpenCV;
+import com.simjessimsol.simcv.nonopencv.opengltracker.OpenGLTracker;
 
 /**
  * Created by Simen on 25.02.2015.
@@ -19,11 +29,13 @@ public class MainMenuActivity extends Activity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private Boolean isNative = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.container);
 
         //improves performance if we know that changes in content doesn't change layout
         mRecyclerView.setHasFixedSize(true);
@@ -32,12 +44,50 @@ public class MainMenuActivity extends Activity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        String[] dataSet = {"Simen", "Rikard", "Christer"};
-
+        String[] dataSet = {"Face Detection", "Circle Detection", "Foreground Detection", "Color Detection"};
+        ToggleButton toggleNative = (ToggleButton) findViewById(R.id.toggleNative);
+        //toggleNative.setClickable(isNative);
 
         //specify the adapter we want to use
-        mAdapter = new MainMenuAdapter(MainMenuActivity.this, dataSet);
+        mAdapter = new MainMenuAdapter(MainMenuActivity.this, dataSet, isNative);
         mRecyclerView.setAdapter(mAdapter);
     }
+
+    public void startFunctionClick(View view) {
+        TextView txtView = (TextView) findViewById(R.id.title);
+        String txtFunction = txtView.getText().toString();
+
+        Toast.makeText(getApplicationContext(), txtView.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    /*public void startFaceDetectionClick(View view) {
+        Intent intent = new Intent(this, FaceDetection.class);
+        startActivity(intent);
+    }
+
+    public void startCircleDetectionClick(View view) {
+        Intent intent = new Intent(this, CircleDetection.class);
+        startActivity(intent);
+    }
+
+    public void startDrawingClick(View view) {
+        Intent intent = new Intent(this, Drawtivity.class);
+        startActivity(intent);
+    }
+
+    public void startForegroundDetection(View view) {
+        Intent intent = new Intent(this, foregroundDetection.class);
+        startActivity(intent);
+    }
+
+    public void startNonCVColorTracking(View view) {
+        Intent intent = new Intent(this, ColorTrackerNonOpenCV.class);
+        startActivity(intent);
+    }
+
+    public void startOpenGlTracker(View view) {
+        Intent intent = new Intent(this, OpenGLTracker.class);
+        startActivity(intent);
+    }*/
 
 }
--- SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/MainMenuAdapter.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/MainMenuAdapter.java
@@ -12,32 +12,34 @@ import com.squareup.picasso.Picasso;
 /**
  * Created by Simen on 25.02.2015.
  */
-public class MainMenuAdapter extends RecyclerView.Adapter<FunctionRowHolder> {
-    private String[] mDataset;
-    private Context mContext;
+public class MainMenuAdapter extends RecyclerView.Adapter<CardViewHolder> {
+    private String[] dataSet;
+    private Context context;
+    private Boolean isNative;
 
-    public MainMenuAdapter(Context context, String[] myDataSet){
-        mContext = context;
-        mDataset = myDataSet;
+    public MainMenuAdapter(Context context, String[] dataSet, Boolean isNative){
+        this.context = context;
+        this.dataSet = dataSet;
+        this.isNative = isNative;
     }
 
     @Override
-    public FunctionRowHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.menu_fragment_card_view, null);
-        FunctionRowHolder rh = new FunctionRowHolder(v);
+    public CardViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.menu_card_view, null);
+        CardViewHolder rh = new CardViewHolder(v);
         return rh;
     }
 
     @Override
-    public void onBindViewHolder(FunctionRowHolder functionRowHolder, int holderPosition) {
-        Picasso.with(mContext).load(R.drawable.ic_launcher)
-                .into(functionRowHolder.thumbnail);
-        functionRowHolder.title.setText(mDataset[holderPosition]);
+    public void onBindViewHolder(CardViewHolder cardViewHolder, int i) {
+        Picasso.with(context).load(R.drawable.ic_launcher)
+                .into(cardViewHolder.thumbnail);
+        cardViewHolder.title.setText(dataSet[i]);
 
     }
 
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return dataSet.length;
     }
 }
