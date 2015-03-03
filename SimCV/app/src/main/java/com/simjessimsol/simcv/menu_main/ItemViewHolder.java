1de5701b2b7e731d7c0051afae96205159a03015
solfuglen <simen.sollie@gmail.com>
2015-03-03 15:19:53 +0100

    fixed colors and layout

--- SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/ItemViewHolder.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/ItemViewHolder.java
@@ -4,6 +4,7 @@ import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -13,17 +14,17 @@ import com.simjessimsol.simcv.R;
 /**
  * Created by Simen on 25.02.2015.
  */
-public class CardViewHolder extends RecyclerView.ViewHolder {
+public class ItemViewHolder extends RecyclerView.ViewHolder {
     protected ImageView thumbnail;
     protected TextView title;
     protected ToggleButton toggleNative;
-    protected Button btnStart;
+    protected ImageButton btnStart;
 
-    public CardViewHolder(View v) {
+    public ItemViewHolder(View v) {
         super(v);
         this.thumbnail = (ImageView) v.findViewById(R.id.thumbnail);
         this.title = (TextView) v.findViewById(R.id.title);
         this.toggleNative = (ToggleButton) v.findViewById(R.id.toggleNative);
-        this.btnStart = (Button) v.findViewById(R.id.btnStart);
+        this.btnStart = (ImageButton) v.findViewById(R.id.btnStart);
     }
 }
--- SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/MainMenuAdapter.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/menu_main/MainMenuAdapter.java
@@ -12,7 +12,7 @@ import com.squareup.picasso.Picasso;
 /**
  * Created by Simen Sollie on 25.02.2015.
  */
-public class MainMenuAdapter extends RecyclerView.Adapter<CardViewHolder> {
+public class MainMenuAdapter extends RecyclerView.Adapter<ItemViewHolder> {
     private String[] dataSet;
     private Context context;
     private Boolean isNative;
@@ -30,26 +30,29 @@ public class MainMenuAdapter extends RecyclerView.Adapter<CardViewHolder> {
     }
 
     @Override
-    public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.activity_menu_recycler_view, null);
-        CardViewHolder vh = new CardViewHolder(v);
+        ItemViewHolder vh = new ItemViewHolder(v);
 
         return vh;
     }
 
     @Override
-    public void onBindViewHolder(CardViewHolder cardViewHolder, final int position) {
+    public void onBindViewHolder(ItemViewHolder itemViewHolder, final int position) {
         Picasso.with(context).load(R.drawable.ic_launcher)
-                .into(cardViewHolder.thumbnail);
-        cardViewHolder.title.setText(dataSet[position]);
-        //cardViewHolder.toggleNative.setVisibility(View.INVISIBLE);
-        cardViewHolder.btnStart.setOnClickListener(new View.OnClickListener() {
+                .into(itemViewHolder.thumbnail);
+        Picasso.with(context).load(R.drawable.ic_action_play)
+                .into(itemViewHolder.btnStart);
+        itemViewHolder.title.setText(dataSet[position]);
+        itemViewHolder.btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mOnItemClickListener.onItemClick(v, position);
             }
         });
+
+
     }
 
     @Override
