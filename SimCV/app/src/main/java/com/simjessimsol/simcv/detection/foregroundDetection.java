662f62cc7837d9838d3b4b03c10c395f6c819796
Simon Jespersen <s.jespersen@stud.uis.no>
2015-04-07 16:11:37 +0200

    fixed stuff

--- SimCV/app/src/main/java/com/simjessimsol/simcv/detection/foregroundDetection.java
+++ SimCV/app/src/main/java/com/simjessimsol/simcv/detection/foregroundDetection.java
@@ -24,11 +24,7 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.video.BackgroundSubtractorMOG;
-
-import java.io.File;
 
 
 public class ForegroundDetection extends Activity implements CvCameraViewListener2 {
